13245a432350df31a575e2b8b04ab395ec74d431
aulusuf <aulusuf@gmail.com>
2019-12-05 12:52:40 +0700

    modify node module

--- public/js/min.js
+++ public/js/min.js
@@ -104,7 +104,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'C:\\project\\perancangan-web\\perancangan-web - Copy\\resources\\js\\min.js'");
 
 /***/ }),
 
