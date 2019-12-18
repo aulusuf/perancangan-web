9cdd8dcba60997ad1440c2ca793aa1371fb6bc4b
aulusuf <aulusuf@gmail.com>
2019-12-18 19:24:45 +0700

    push new

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,41 @@
-// $("#toggle_modal").click(function(){
-//     $("#modal").removeClass('hidden');
-// })
-
-// $("#modal_overlay").click(function () { 
-//     $("#modal").addClass('hidden');    
-// });
-
-// $("#modal_cancel").click(function (event) { 
-//     $("#modal").removeClass('hidden');
-// });
 
 
-
-
+// Modal
 $("#toggle_modal").click(function(){
-    $("#modal").show();
+    $("#modal").fadeIn();
 })
 
 $("#modal_overlay").click(function () { 
-    $("#modal").hide();    
+    $("#modal").fadeout();    
 });
 
 $("#modal_cancel").click(function () { 
-    $("#modal").hide();
+    $("#modal").fadeOut();
 });
+
+// Validasi
+
+$.fn.validasi = function () {
+    var form = this;
+
+    this.on("submit", function (event) {
+        
+        event.preventDefault();
+        var email = $("input[name='email']").val();
+        var password = $("input[name='password']").val();
+
+        if (email == "" || password == "") {
+            $("#email").addClass("border-red-500");
+            $("#password").addClass("border-red-500");
+            alert("Fill the input email and password field to login");          
+            // show_error('Harap isi email dan password untuk login');
+        } else if (email != "email" && password != "password") {
+            $("#email").alert("Wrong email or password");
+
+            // show_error('Email atau Password salah. Coba lagi');
+        } else {
+            // document.location.href = "dashboard.html";
+            $(this).off("submit").trigger("submit");
+        }
+    });
+}    
